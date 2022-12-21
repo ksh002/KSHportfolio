3c20a9ab397e6f5168d8c928104b75aa27c2325d
강성훈 <uio6625@naver.com>
2022-12-21 17:01:38 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -2,11 +2,20 @@ $(function(){
 
 
     $(".skillBox .skill_left ul li").click(function(){
+        let i = $(this).index();
+
         $(this).addClass('active')
         $(this).siblings().removeClass('active')
-        console.log("아아")
+        $(".rightBox").removeClass("active").eq(i).addClass("active")
+        // $(".rightHtml").css("margin-top", i*60+"px")
     })
 
+    $(".skill-box .left-side ul li").click(function(){
+        let i = $(this).index();
+        $(".skill-box .left-side ul li").removeClass("active").eq(i).addClass("active")
+        $(".right-side > div").removeClass("active").eq(i).addClass("active")
+        $(".rightHtml").css("margin-top", i*60+"px")
+    })
 
 
 
