2c736e997f90eaef9a8d0a7d4d2b2faeda3e42dd
강성훈 <uio6625@naver.com>
2022-12-21 14:53:19 +0900

    21days commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function(){
+
+
+    $(".skillBox .skill_left ul li").click(function(){
+        $(this).addClass('active')
+        $(this).siblings().removeClass('active')
+        console.log("아아")
+    })
+
+
+
+
+
+
+
+
+
+
+
+})
