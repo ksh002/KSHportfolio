6ab55777d856284f900946fed90f1078df19f33f
강성훈 <uio6625@naver.com>
2022-12-22 16:04:17 +0900

    fix all

--- js/script.js
+++ js/script.js
@@ -1,24 +1,41 @@
 $(function(){
 
+    // 메인페이지 커서 깜빡임 효과
+    function blink(){
+        target.classList.toggle("active");
+    }
+    setInterval(blink, 500);
 
-    $(".skillBox .skill_left ul li").click(function(){
+    // Designed By Sung Hoon 타이핑 애니메이션
+    let target = document.querySelector("#dynamic");
+    let stringText = "Designed By Sung Hoon";
+    let selectStringArr = stringText.split("");
+
+    function dynamic(randomArr){
+        if(randomArr.length > 0){
+            target.textContent += randomArr.shift();
+            setTimeout(function(){
+                dynamic(randomArr);
+            },80)
+        }
+    }
+    dynamic(selectStringArr);
+    
+
+    
+    
+
+    // skillBox의 탭 이동 및 막대바 애니메이션 효과
+    $(".skillBox .skill_left ul li").click(function(){ 
         let i = $(this).index();
 
-        $(this).addClass('active')
-        $(this).siblings().removeClass('active')
-        $(".rightBox").removeClass("active").eq(i).addClass("active")
-        // $(".rightHtml").css("margin-top", i*60+"px")
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+        $(".rightBox").removeClass("active").eq(i).addClass("active");
+        $(".border .line").css("margin-top", i*40+"px");
     })
 
-    $(".skill-box .left-side ul li").click(function(){
-        let i = $(this).index();
-        $(".skill-box .left-side ul li").removeClass("active").eq(i).addClass("active")
-        $(".right-side > div").removeClass("active").eq(i).addClass("active")
-        $(".rightHtml").css("margin-top", i*60+"px")
-    })
-
-
-
+    
 
 
 
