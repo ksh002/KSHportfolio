541384166e024565ef10002f1af7e1a46ef2bcd7
강성훈 <uio6625@naver.com>
2022-12-26 20:15:23 +0900

    swiper update > design

--- js/web.js
+++ js/web.js
@@ -6,36 +6,36 @@ $(function(){
         // console.log(height);
 
         if(height < 533){
-            $(".rnLeft_box > div ul li:first-child").addClass("on");
-            $(".rnLeft_box > div ul li:first-child").siblings().removeClass("on");
+            $(".wdLeft_box > div ul li:first-child").addClass("on");
+            $(".wdLeft_box > div ul li:first-child").siblings().removeClass("on");
         }
         else if(533 < height < 1467){
-            $(".rnLeft_box > div ul li:nth-child(2)").addClass("on");
-            $(".rnLeft_box > div ul li:nth-child(2)").siblings().removeClass("on");
+            $(".wdLeft_box > div ul li:nth-child(2)").addClass("on");
+            $(".wdLeft_box > div ul li:nth-child(2)").siblings().removeClass("on");
         }
 
         if(height > 1467){
-            $(".rnLeft_box > div ul li:last-child").addClass("on");
-            $(".rnLeft_box > div ul li:last-child").siblings().removeClass("on");
+            $(".wdLeft_box > div ul li:last-child").addClass("on");
+            $(".wdLeft_box > div ul li:last-child").siblings().removeClass("on");
         }
     });
 
 
     // leftBox의 li 클릭했을때 오른쪽화면이 지정된 scroll좌표위치로 이동하는 스크립트
-    $("#web .renewal .rnLeft_box > div ul li:first-child").click(function(){
+    $("#web .renewal .wdLeft_box > div ul li:first-child").click(function(){
         $("html").animate({scrollTop : "0"}, 500);
     });
 
-    $("#web .renewal .rnLeft_box > div ul li:nth-child(2)").click(function(){
+    $("#web .renewal .wdLeft_box > div ul li:nth-child(2)").click(function(){
         $("html").animate({scrollTop : "970"}, 500);
     });
 
-    $("#web .renewal .rnLeft_box > div ul li:last-child").click(function(){
+    $("#web .renewal .wdLeft_box > div ul li:last-child").click(function(){
         $("html").animate({scrollTop : "1937"}, 500);
     })
 
 
-
+    
 
 
 
